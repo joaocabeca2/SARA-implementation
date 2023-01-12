07d41ecad6beb3308cf9fa5afef2f19f1f6f82d2
João Pedro Felicio Pereira <72157587+joaocabeca2@users.noreply.github.com>
2023-01-12 11:33:15 -0300

    Update sara.py

--- sara.py
+++ sara.py
@@ -8,10 +8,11 @@ class SARA(IR2A):
         IR2A.__init__(self,id)
         self.throughputs = []
         self.qi = []
+        self.segments_size = []
         self.request_time = 0
-        self.response_time = 0
-        self.parsed_mpd = None
+        self.hn = 1 #media harmonica
         self.bcurr = 0 # ocupaçao do buffer
+        self.bmax = self.whiteboard.get_max_buffer_size()
         
     #requisições são movidas de cima para baixo
     def handle_xml_request(self, msg):
@@ -20,41 +21,53 @@ class SARA(IR2A):
         
     #respostas são movidas de baixo para cima
     def handle_xml_response(self, msg):
-        self.parsed_mpd = parse_mpd(msg.get_payload())
-        self.qi = self.parsed_mpd.get_qi()
-        self.response_time = time.perf_counter()
+        parsed_mpd = parse_mpd(msg.get_payload())
+        self.qi = parsed_mpd.get_qi()
 
         #tempo de do envio do request ate receber a resposta
-        t = self.response_time - self.request_time
-
-        #adicionando as taxas de transferencia na lista
-        self.throughputs.append(msg.get_bit_length()/t)
+        t = time.perf_counter() - self.request_time
 
         self.send_up(msg)
     
     def handle_segment_size_request(self, msg):
-        selected_qi = 0
-        self.request_time = time.perf_counter()
+        selected_qi = self.qi[0]
         self.bcurr = self.whiteboard.get_amount_video_to_play()
+        beta = self.bmax * 0.8
+        balfa = self.bmax * 0.4
+        i = self.bmax * 0.15
+
+        self.request_time = time.perf_counter()
+
+        #FAST START
+        if self.bcurr <= i:
+            selected_id = min(self.qi)
+        else:
+            pass
         
-        #FAST START –  Bcurr <= I
-        if self.bcurr <= msg.get_bit_length():
-            #print(f"Menor taxa de bits = {min(self.throughputs)}")
-            selected_qi = min(self.qi)
-        
-        msg.add_quality_id(self.qi[0])
+        msg.add_quality_id(selected_id)
 
         self.send_down(msg)
     
     def handle_segment_size_response(self, msg):
-        self.response_time = time.perf_counter()
-
         #tempo de do envio do request ate receber a resposta
-        t = self.response_time - self.request_time
-        #print('>>>>>>>>>>>>>>>',msg.get_bit_length())
+        t = time.perf_counter() - self.request_time
+        segment_size = msg.get_bit_length()
+
+        print('BUFFER SIZE',self.bcurr)
+        print('MAXIMO BUFFER',self.bmax)
+        print('MEDIA HARMONICA',self.hn)
+
+        self.throughputs.append(segment_size/t)
+        self.segments_size.append(segment_size) 
+        self.hn += self.calculate_harmonic_mean(t,segment_size)
 
         self.send_up(msg)
     
+    def calculate_harmonic_mean(self,t,segment_size):
+        wi = segment_size*len(self.segments_size)+1
+        di = segment_size*(len(self.segments_size)/t)
+        return wi/di
+
     def initialize(self):
         pass
 
